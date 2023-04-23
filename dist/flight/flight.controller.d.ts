86bd7b59f95e2a13fb2cc7d1757c5c2db90d344e
Ramon K. Shin <51011193+ramer-dev@users.noreply.github.com>
2023-04-23 12:57:36 +0900

    Delete dist directory

--- dist/flight/flight.controller.d.ts
+++
@@ -1,6 +0,0 @@
-import { FlightService } from './flight.service';
-export declare class FlightController {
-    private readonly flightService;
-    constructor(flightService: FlightService);
-    getHello(): string;
-}
