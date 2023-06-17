6733ec9476f8ff94994758a927573143f0d8d5b9
ramer-dev <gml4009@gmail.com>
2023-06-17 17:08:42 +0900

    feat : Map API Update

--- src/domain/map/map.controller.ts
+++ src/domain/map/map.controller.ts
@@ -2,7 +2,6 @@ import { Controller, Get, Param, Res } from "@nestjs/common";
 import { ApiTags } from '@nestjs/swagger';
 import { InsertFrequencyDto } from 'common/dto/frequency/frequency.insert.dto';
 import { UpdateFrequencyDto } from 'common/dto/frequency/frequency.update.dto';
-import { MapService } from './map.service';
 import { join } from "path";
 import fs from "fs";
 import { Response } from "express";
@@ -10,7 +9,6 @@ import { Response } from "express";
 @Controller('map')
 @ApiTags('지도 API')
 export class MapController {
-  constructor(private readonly mapService: MapService) {}
 
   @Get(':z/:x/:y')
   getSingleMap(
--- src/domain/map/map.module.ts
+++ src/domain/map/map.module.ts
@@ -2,10 +2,8 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Frequency } from 'entities/frequency.entity';
 import { MapController } from './map.controller';
-import { MapService } from './map.service';
 
 @Module({
   controllers: [MapController],
-  providers: [MapService],
 })
 export class MapModule {}
